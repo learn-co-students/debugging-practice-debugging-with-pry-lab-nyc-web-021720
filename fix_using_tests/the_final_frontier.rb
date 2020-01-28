72627683eff07ac346d474c3423c861fc4439b6b
Samantha Ponce <samanthaponce5@gmail.com>
2020-01-28 15:44:15 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,9 +12,12 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
+
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
-end
+
+  crew.map {|crew_member| "Hello #{crew_member}."} 
+  end
+
 
 def engage
   date = generate_star_date
